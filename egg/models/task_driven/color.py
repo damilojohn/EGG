105e25859f8dc3b66d4585920163ba4949110e2c
Damilola John <oduguwadamilola40@gmail.com>
2024-04-30 14:26:02 +0100

    added models

---
+++ egg/models/task_driven/color.py
@@ -0,0 +1,14 @@
+import torch
+import lib
+
+
+def get_color_task_driven_ensemble():
+    pass
+
+
+def get_colorgray_task_driven_ensemble():
+    pass
+
+
+def get_gray_task_driven_ensemble():
+    pass
